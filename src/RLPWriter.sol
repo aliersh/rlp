bc238084193562438d3bad259f5b243198e7dc41
aliersh <ariel.diaz.borquez@icloud.com>
2025-03-06 12:42:10 -0500

    fix(test): correct initialization and length calculation in < 55 bytes lists RLP tests
    - initialize decodedList with proper size
    - use bytesPayload.length instead of payload.length for correct length byte calculation
    - fix typo in lengthLengthBytes variable

--- src/RLPWriter.sol
+++ src/RLPWriter.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-library RLPWriter { }
+library RLPWriter {
+    function writeBytes(bytes memory _in) internal pure returns (bytes memory) {
+        // TODO
+    }
+
+    function writeList(bytes[] memory _in) internal pure returns (bytes memory) {
+        // TODO
+    }
+}
